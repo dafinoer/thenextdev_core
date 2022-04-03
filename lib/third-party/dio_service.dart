ccf8dabe47371d130483873395803c8aca7e90ec
dafinoer <dafinoer@outlook.com>
2022-04-03 14:39:55 +0700

    change transform

--- lib/third-party/dio_service.dart
+++ lib/third-party/dio_service.dart
@@ -1,22 +1,24 @@
 import 'package:dio/dio.dart';
 import '../../service_setup.dart';
+import 'dio_transform.dart';
 
 class DioService {
+  final Dio _dio;
+
+  DioService._(this._dio);
+
   static late final Dio service;
-  final Dio dio;
-  DioService._(this.dio);
 
   factory DioService.create(ServiceSetup setup) {
     final option = BaseOptions(
-      baseUrl: setup.baseUrl,
-      connectTimeout: setup.connectTimeOut,
-      receiveTimeout: setup.receiveTimeOut
-    );
+        baseUrl: setup.baseUrl,
+        connectTimeout: setup.connectTimeOut,
+        receiveTimeout: setup.receiveTimeOut);
     service = Dio(option);
-    
+    service.transformer = TheNextDevTransform();
     return DioService._(service);
   }
 
   void addInterceptors(List<Interceptor> interceptors) =>
-      dio.interceptors.addAll(interceptors);
+      _dio.interceptors.addAll(interceptors);
 }
